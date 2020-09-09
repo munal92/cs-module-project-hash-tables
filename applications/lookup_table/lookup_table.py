cd5917b8aa30901e3d1e19f92234a14d1c1143d5
unknown <m_fattih@hotmail.com>
2020-09-08 20:42:22 -0700

    day 3

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,4 +1,7 @@
 # Your code here
+import random
+import math
+import timeit
 
 
 def slowfun_too_slow(x, y):
@@ -9,6 +12,10 @@ def slowfun_too_slow(x, y):
 
     return v
 
+
+cache_dict = {}
+
+
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
@@ -16,11 +23,26 @@ def slowfun(x, y):
     """
     # Your code here
 
+    key_ = str(x) + ','+str(y)
+    if key_ in cache_dict:
+        # print("used cache")
+        return cache_dict[key_]
+    else:
+        v = math.pow(x, y)
+        v = math.factorial(v)
+        v //= (x + y)
+        v %= 982451653
+        cache_dict[key_] = v
+        return v
 
 
 # Do not modify below this line!
-
-for i in range(50000):
+start_time = timeit.default_timer()
+for i in range(50000):  # 50000
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
-    print(f'{i}: {x},{y}: {slowfun(x, y)}')
+    slowfun(x, y)
+    # print(f'{i}: {x},{y}: {slowfun(x, y)}')
+
+elapsed = timeit.default_timer() - start_time
+print(f'done {elapsed}')
